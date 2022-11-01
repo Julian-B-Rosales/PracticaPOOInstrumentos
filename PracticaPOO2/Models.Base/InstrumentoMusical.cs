f43e1f91071501bceec8ae6a53af02a5caacdffc
julianRosales <rjulianbenjamin@gmail.com>
2022-11-01 16:18:20 -0300

    Aclaracion

--- PracticaPOO2/Models.Base/InstrumentoMusical.cs
+++ PracticaPOO2/Models.Base/InstrumentoMusical.cs
@@ -8,6 +8,7 @@ namespace PracticaPOO2.Models.Base
 {
     public abstract class InstrumentoMusical
     {
+        //Se hace que la clase devuelva string en lugar de void para poder mostrar en MessageBox y no solo en consola
         public abstract string Afinar();
     }
 }
